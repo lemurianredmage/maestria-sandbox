0009709a95873faccaf805fc74f73d06747fb422
Lemurian TechPriest <lemurianredmage@hotmail.com>
2024-11-01 16:24:00 -0700

    Updating files

--- utils/datasets.py
+++ utils/datasets.py
@@ -3,14 +3,7 @@ import tarfile
 import urllib.request
 import pandas as pd
 
-def download(data_path: str, tgz_path: str, data_url: str):
-    os.makedirs(data_path, exist_ok=True)
-    urllib.request.urlretrieve(data_url, tgz_path)
-    downloaded_tgz = tarfile.open(tgz_path)
-    downloaded_tgz.extractall(path=data_path)
-    downloaded_tgz.close()
-
-def download(url: str, download_path: str, files: list):
+def download(url: str, download_path: str, files: list, decompress_type: str = 'none'):
     os.makedirs(download_path, exist_ok=True)
     for filename in (files):
         filepath = os.path.join(download_path, filename)
@@ -18,9 +11,20 @@ def download(url: str, download_path: str, files: list):
             print("Downloading", filename)
             urllib.request.urlretrieve(url + filename, filepath)
             print(f"{filename} download was completed.")
+            decompress(decompress_type, download_path, filepath)
         else:
             print(f"{filename} is already downloaded.")
 
 def load_csv(data_path, filename):
     csv_path = os.path.join(data_path, filename)
     return pd.read_csv(csv_path)
+
+def decompress(type: str, download_path: str, filePath: str):
+    match type:
+        case 'bz2':
+            print("Decompressing files ...")
+            compressed_file = tarfile.open(filePath)
+            compressed_file.extractall(path=download_path)
+            compressed_file.close()
+        case _:
+            return
--- utils/helpers.py
+++ utils/helpers.py
@@ -4,5 +4,4 @@ def calculate_execution_time(start_time, end_time):
     # Calculate minutes and seconds
     minutes = execution_time // 60  # Integer division for whole minutes
     seconds = execution_time % 60   # Remainder for leftover seconds
-    print(f"Total execution time: {execution_time:.2f} seconds")
     print(f"Total execution time: {minutes} minutes and {seconds:.2f} seconds")
