a7ef6245a7a6960fbe69eb71836316810e378d74
Lemurian TechPriest <lemurianredmage@hotmail.com>
2024-12-01 23:01:18 -0800

    added: New PoC notebooks

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,3 +1,5 @@
+import os
+import shutil
 
 def calculate_execution_time(start_time, end_time):
     execution_time = end_time - start_time
@@ -5,3 +7,11 @@ def calculate_execution_time(start_time, end_time):
     minutes = execution_time // 60  # Integer division for whole minutes
     seconds = execution_time % 60   # Remainder for leftover seconds
     print(f"Total execution time: {minutes} minutes and {seconds:.2f} seconds")
+
+
+def setup_workdir(working_path):
+    if not os.path.exists(working_path):
+        os.makedirs(working_path)
+    else:
+        shutil.rmtree(working_path)
+        os.makedirs(working_path)
