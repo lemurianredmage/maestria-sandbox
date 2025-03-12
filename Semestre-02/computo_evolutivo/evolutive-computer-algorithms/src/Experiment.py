69aa933b9c5907fdf172855fe597548fca6048b1
Yovanny Nogales <nverayovanny@gmail.com>
2025-03-12 09:22:14 -0700

    Add logic to generate stats for all groups of problems

--- Semestre-02/computo_evolutivo/evolutive-computer-algorithms/src/Experiment.py
+++ Semestre-02/computo_evolutivo/evolutive-computer-algorithms/src/Experiment.py
@@ -1,6 +1,7 @@
 from equations import Equations as eq
 from methods.BaseMethod import BaseMethod as base
 import pandas as pd
+import numpy as np
 
 class Experiment:
     @staticmethod
@@ -56,9 +57,132 @@ class Experiment:
         return stats
 
     @staticmethod
+    def experimentForTwoDimensions(number_of_experiments, number_of_executions):
+        number_of_tweaks = int(number_of_executions / 4)
+        interval = [int(number_of_executions/5), int(number_of_executions/2)]
+
+        parameter_list = [{
+            'func': eq.F7,
+            'maxStep': 20,
+            'number_experiments': number_of_experiments,
+            'number_of_executions': number_of_executions,
+            'number_of_dimensions': 2,
+            'number_of_tweaks': number_of_tweaks,
+            'interval': interval,
+            'minDomainValue': -20,
+            'maxDomainValue': 20,
+            'temperature': 1000,
+            'temperatureDecrease': 100,
+            'maximize': False
+        }, {
+            'func': eq.F12,
+            'maxStep': 20,
+            'number_experiments': number_of_experiments,
+            'number_of_executions': number_of_executions,
+            'number_of_dimensions': 2,
+            'number_of_tweaks': number_of_tweaks,
+            'interval': interval,
+            'minDomainValue': -20,
+            'maxDomainValue': 20,
+            'temperature': 1000,
+            'temperatureDecrease': 100,
+            'maximize': False
+        }, {
+            'func': eq.F13,
+            'maxStep': 20,
+            'number_experiments': number_of_experiments,
+            'number_of_executions': number_of_executions,
+            'number_of_dimensions': 2,
+            'number_of_tweaks': number_of_tweaks,
+            'interval': interval,
+            'minDomainValue': -10,
+            'maxDomainValue': 10,
+            'temperature': 1000,
+            'temperatureDecrease': 100,
+            'maximize': False
+        }]
+
+        stats = Experiment.concat_stats(parameter_list)
+        fileName = 'two_dimensions'
+        stats.to_csv(f'{fileName}.csv', index=False)
+
+        return stats
+    
+    @staticmethod
+    def experimentForMultipleDimensions(number_of_experiments, number_of_executions, dimensions = [2, 5, 10, 100, 1000]):
+        number_of_tweaks = int(number_of_executions / 4)
+        interval = [int(number_of_executions/5), int(number_of_executions/2)]
+
+        parameter_list = []
+
+        for dimension in dimensions:
+            parameter_list = parameter_list + [{
+                'func': eq.F3,
+                'maxStep': 20,
+                'number_experiments': number_of_experiments,
+                'number_of_executions': number_of_executions,
+                'number_of_dimensions': dimension,
+                'number_of_tweaks': number_of_tweaks,
+                'interval': interval,
+                'minDomainValue': -20,
+                'maxDomainValue': 20,
+                'temperature': 1000,
+                'temperatureDecrease': 100,
+                'maximize': False
+            }, {
+                'func': eq.F4,
+                'maxStep': 20,
+                'number_experiments': number_of_experiments,
+                'number_of_executions': number_of_executions,
+                'number_of_dimensions': dimension,
+                'number_of_tweaks': number_of_tweaks,
+                'interval': interval,
+                'minDomainValue': -20,
+                'maxDomainValue': 20,
+                'temperature': 1000,
+                'temperatureDecrease': 100,
+                'maximize': False
+            }, {
+                'func': eq.F5,
+                'maxStep': 20,
+                'number_experiments': number_of_experiments,
+                'number_of_executions': number_of_executions,
+                'number_of_dimensions': dimension,
+                'number_of_tweaks': number_of_tweaks,
+                'interval': interval,
+                'minDomainValue': -10,
+                'maxDomainValue': 10,
+                'temperature': 1000,
+                'temperatureDecrease': 100,
+                'maximize': False
+            }, {
+                'func': eq.F11,
+                'maxStep': 20,
+                'number_experiments': number_of_experiments,
+                'number_of_executions': number_of_executions,
+                'number_of_dimensions': dimension,
+                'number_of_tweaks': number_of_tweaks,
+                'interval': interval,
+                'minDomainValue': -10,
+                'maxDomainValue': 10,
+                'temperature': 1000,
+                'temperatureDecrease': 100,
+                'maximize': False
+            }]
+
+        stats = Experiment.concat_stats(parameter_list)
+        fileName = 'multiple_dimensions'
+        stats.to_csv(f'{fileName}.csv', index=False)
+
+        return stats
+
+    @staticmethod
     def concat_stats(parameter_list):
         stats = []
         for experiment_data in parameter_list:
+            hyperparameters = experiment_data.copy()
+            del hyperparameters['func']
+
             method_instance = base(minDomainValue=experiment_data['minDomainValue'], maxDomainValue=experiment_data['maxDomainValue'])
             hill_climbing_best_result, hill_climbing_convergence_list = method_instance.HillClimbing(
                 experiment_data['func'],
@@ -69,11 +193,12 @@ class Experiment:
             )
 
             stats.append({
-                'func': experiment_data['func'],
+                'func': experiment_data['func'].__name__,
                 'method': 'hill_climbing',
-                'hyperparameters': experiment_data,
-                'best_result': hill_climbing_best_result,
-                'convergence_list': hill_climbing_convergence_list
+                'hyperparameters': hyperparameters,
+                'number_of_dimensions': hyperparameters['number_of_dimensions'],
+                'convergence_list': hill_climbing_convergence_list,
+                'best_result': hill_climbing_best_result[0],
             })
 
             steepest_ascent_hill_climbing_best_result, steepest_ascent_hill_climbing_convergence_list = method_instance.SteepestAscentHillClimbing(
@@ -86,11 +211,12 @@ class Experiment:
             )
 
             stats.append({
-                'func': experiment_data['func'],
+                'func': experiment_data['func'].__name__,
                 'method': 'steepest_ascent_hill_climbing',
-                'hyperparameters': experiment_data,
-                'best_result': steepest_ascent_hill_climbing_best_result,
-                'convergence_list': steepest_ascent_hill_climbing_convergence_list
+                'hyperparameters': hyperparameters,
+                'number_of_dimensions': hyperparameters['number_of_dimensions'],
+                'convergence_list': steepest_ascent_hill_climbing_convergence_list,
+                'best_result': steepest_ascent_hill_climbing_best_result[0],
             })
 
             steepest_ascent_hill_climbing_with_replacement_best_result, steepest_ascent_hill_climbing_with_replacement_convergence_list = method_instance.SteepestAscentHillClimbingWithReplacement(
@@ -103,11 +229,12 @@ class Experiment:
             )
 
             stats.append({
-                'func': experiment_data['func'],
+                'func': experiment_data['func'].__name__,
                 'method': 'steepest_ascent_hill_climbing_with_replacement',
-                'hyperparameters': experiment_data,
-                'best_result': steepest_ascent_hill_climbing_with_replacement_best_result,
-                'convergence_list': steepest_ascent_hill_climbing_with_replacement_convergence_list
+                'hyperparameters': hyperparameters,
+                'number_of_dimensions': hyperparameters['number_of_dimensions'],
+                'convergence_list': steepest_ascent_hill_climbing_with_replacement_convergence_list,
+                'best_result': steepest_ascent_hill_climbing_with_replacement_best_result[0],
             })
 
             random_search_best_result, random_search_convergence_list = method_instance.RandomSearch(
@@ -118,11 +245,12 @@ class Experiment:
             )
 
             stats.append({
-                'func': experiment_data['func'],
+                'func': experiment_data['func'].__name__,
                 'method': 'random_search',
-                'hyperparameters': experiment_data,
-                'best_result': random_search_best_result,
-                'convergence_list': random_search_convergence_list
+                'hyperparameters': hyperparameters,
+                'number_of_dimensions': hyperparameters['number_of_dimensions'],
+                'convergence_list': random_search_convergence_list,
+                'best_result': random_search_best_result[0],
             })
 
             hill_climbing_with_random_restarts_best_result, hill_climbing_with_random_restarts_convergence_list = method_instance.HillClimbingWithRandomRestarts(
@@ -135,11 +263,12 @@ class Experiment:
             )
 
             stats.append({
-                'func': experiment_data['func'],
+                'func': experiment_data['func'].__name__,
                 'method': 'hill_climbing_with_random_restarts',
-                'hyperparameters': experiment_data,
-                'best_result': hill_climbing_with_random_restarts_best_result,
-                'convergence_list': hill_climbing_with_random_restarts_convergence_list
+                'hyperparameters': hyperparameters,
+                'number_of_dimensions': hyperparameters['number_of_dimensions'],
+                'convergence_list': hill_climbing_with_random_restarts_convergence_list,
+                'best_result': hill_climbing_with_random_restarts_best_result[0],
             })
 
             simmulated_anneling_best_result, simmulated_anneling_convergence_list = method_instance.SimmulatedAnneling(
@@ -153,11 +282,12 @@ class Experiment:
             )
 
             stats.append({
-                'func': experiment_data['func'],
+                'func': experiment_data['func'].__name__,
                 'method': 'simmulated_anneling',
-                'hyperparameters': experiment_data,
-                'best_result': simmulated_anneling_best_result,
-                'convergence_list': simmulated_anneling_convergence_list
+                'hyperparameters': hyperparameters,
+                'number_of_dimensions': hyperparameters['number_of_dimensions'],
+                'convergence_list': simmulated_anneling_convergence_list,
+                'best_result': simmulated_anneling_best_result[0],
             })
 
             iterated_local_search_with_random_restarts_best_result, iterated_local_search_with_random_restarts_convergence_list = method_instance.IteratedLocalSearchWithRandomRestarts(
@@ -170,13 +300,27 @@ class Experiment:
             )
 
             stats.append({
-                'func': experiment_data['func'],
+                'func': experiment_data['func'].__name__,
                 'method': 'iterated_local_search_with_random_restarts',
-                'hyperparameters': experiment_data,
-                'best_result': iterated_local_search_with_random_restarts_best_result,
-                'convergence_list': iterated_local_search_with_random_restarts_convergence_list
+                'hyperparameters': hyperparameters,
+                'number_of_dimensions': hyperparameters['number_of_dimensions'],
+                'convergence_list': iterated_local_search_with_random_restarts_convergence_list,
+                'best_result': iterated_local_search_with_random_restarts_best_result[0],
             })
         
-        return pd.DataFrame(stats)
+        stats = pd.DataFrame(stats)
 
+        Experiment.add_stats_by_rows(stats)
+
+        return stats
+
+    @staticmethod
+    def add_stats_by_rows(df):
+        for index, row in df.iterrows():
+            df.at[index, 'median'] = np.round(np.median(row.loc['convergence_list']), 6)
+            df.at[index, 'mean'] = np.round(np.mean(row.loc['convergence_list']), 6)
+            df.at[index, 'minimum'] = np.round(np.min(row.loc['convergence_list']), 6)
+            df.at[index, 'maximum'] = np.round(np.max(row.loc['convergence_list']), 6)
+            df.at[index, 'std_dev'] = np.round(np.std(row.loc['convergence_list']), 6)
+            df.at[index, 'iqr'] = np.round(np.percentile(row.loc['convergence_list'], 75) - np.percentile(row.loc['convergence_list'], 25), 6)
             
